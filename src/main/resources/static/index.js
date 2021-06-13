822f186f5fc7df3f727ca6a87ea8ca4cacea4316
thealinivan <ivana@roehampton.ac.uk>
2021-06-13 17:29:00 +0100

    HWA-6 AI: Added frontend layout

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,73 @@
+
+//API
+
+//MAPS(POSTCODES) API
+//...
+
+
+//PLACES API
+//..
+
+//Create place fetch
+//...
+//Read place fetch
+//...
+//Update place fetch
+//...
+//Delete place fetch
+//...
+
+
+//CLUBS API
+//Create club fetch
+//...
+//Read clubs fetch
+//...
+//Update club(assign place) fetch
+//...
+//Update club(remove place) fetch
+//Delete club fetch
+//...
+
+
+//RENDER DATA TO DOM
+//PLACES
+//...
+
+//CLUBS
+//...
+
+//GET DATA FROM FORMS
+//PLACES
+//...
+
+//CLUBS
+//...
+
+
+
+//ON DOCUMENT LOAD
+$(window).bind("load", function () {
+
+    //EVENT LISTENERS
+
+    //SEARCH
+    //...
+
+    //PLACES
+    //..
+
+
+    //CLUBS
+    //Create club
+    //...
+    //Update (asign place)
+    //...
+    //Remove (from club)
+    //...
+    //Delete
+    //...
+
+})
+
+
