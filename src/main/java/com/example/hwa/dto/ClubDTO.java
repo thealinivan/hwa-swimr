bbab004f3adbe729c8bb21aa48797f92bd622d13
thealinivan <ivana@roehampton.ac.uk>
2021-06-14 16:17:44 +0100

    HWA-10 AI: Added Place DTO

---
+++ src/main/java/com/example/hwa/dto/ClubDTO.java
@@ -0,0 +1,5 @@
+package com.example.hwa.dto;
+
+public class ClubDTO {
+
+}
---
+++ src/main/java/com/example/hwa/dto/PlaceDTO.java
@@ -0,0 +1,48 @@
+package com.example.hwa.dto;
+
+public class PlaceDTO {
+	private Integer id;
+	private String name;
+	private String postcode;
+	
+	public PlaceDTO() {}
+
+	public PlaceDTO(String name, String postcode) {
+		super();
+		this.name = name;
+		this.postcode = postcode;
+	}
+
+	public PlaceDTO(Integer id, String name, String postcode) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.postcode = postcode;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	};
+	
+	
+}
