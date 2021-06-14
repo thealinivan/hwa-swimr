7b97d08d1472a454bb275d60ade40a1579e66b14
thealinivan <ivana@roehampton.ac.uk>
2021-06-14 16:37:19 +0100

    HWA-10 AI: Added club repo and service

--- src/main/java/com/example/hwa/repo/ClubRepo.java
+++ src/main/java/com/example/hwa/repo/ClubRepo.java
@@ -1,5 +1,11 @@
 package com.example.hwa.repo;
 
-public class ClubRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.hwa.domain.Club;
+
+@Repository
+public interface ClubRepo extends JpaRepository<Club, Integer> {
 
 }
