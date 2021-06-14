2cbfee3c1d064d425ef97da99976fc8597264fc5
thealinivan <ivana@roehampton.ac.uk>
2021-06-14 18:51:06 +0100

    HWA-10 AI: Service setup for database

--- src/main/java/com/example/hwa/service/ClubService.java
+++ src/main/java/com/example/hwa/service/ClubService.java
@@ -1,5 +1,18 @@
 package com.example.hwa.service;
 
-public class ClubService {
+import org.springframework.stereotype.Service;
 
+import com.example.hwa.repo.PlaceRepo;
+import com.example.hwa.utils.PlaceMapper;
+
+@Service
+public class ClubService {
+	private PlaceRepo repo;
+	private PlaceMapper mapper;
+	public ClubService(PlaceRepo repo, PlaceMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+	
 }
--- src/main/java/com/example/hwa/service/PlaceService.java
+++ src/main/java/com/example/hwa/service/PlaceService.java
@@ -2,7 +2,17 @@ package com.example.hwa.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.hwa.repo.ClubRepo;
+import com.example.hwa.utils.ClubMapper;
+
 @Service
 public class PlaceService {
+	private ClubRepo repo;
+	private ClubMapper mapper;
+	public PlaceService(ClubRepo repo, ClubMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
 	
 }
