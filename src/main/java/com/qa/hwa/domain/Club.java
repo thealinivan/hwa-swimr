6067d178d95c6c757bb8241f4e0cef6eecd1630f
thealinivan <ivana@roehampton.ac.uk>
2021-06-20 15:21:29 +0100

    HWA-16 AI: Solved bugs: hashcode and club deletion

--- src/main/java/com/qa/hwa/domain/Club.java
+++ src/main/java/com/qa/hwa/domain/Club.java
@@ -54,6 +54,16 @@ public class Club {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((places == null) ? 0 : places.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -78,10 +88,5 @@ public class Club {
 		} else if (!places.equals(other.places))
 			return false;
 		return true;
-	}
-	
-	
-	
-	
-	
+	}	
 }
